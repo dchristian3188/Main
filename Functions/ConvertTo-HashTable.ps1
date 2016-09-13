23b0ca1c237e6a13f93146c9affaff280f8fd542
dchristian3188 <dchritsian3188@gmail.com>
2016-09-12 22:00:15 -0700

    Adding ConvertTo Hash / WhereString

---
+++ Functions/ConvertTo-HashTable.ps1
@@ -0,0 +1,79 @@
+﻿Function ConvertTo-HashTable
+{
+    <#
+    .Synopsis
+       Converts incoming object to a hashtable.
+    .DESCRIPTION
+       Converts incoming object to a hashtable.
+    .PARAMETER InputObject
+        Object to be converted
+    .PARAMETER IncludeNullValues
+        Optional parameter. If present adds properties 
+        with null values to the hashtable
+    .EXAMPLE
+       Get-Service -Name Spooler | ConvertTo-HashTable
+        Name                           Value                                                                
+        ----                           -----                                                                
+        CanStop                        True                                                                 
+        ServiceName                    Spooler                                                              
+        DisplayName                    Print Spooler                                                        
+        ServiceType                    Win32OwnProcess, InteractiveProcess                                  
+        RequiredServices               {RPCSS, http}                                                        
+        CanPauseAndContinue            False                                                                
+        Name                           Spooler                                                              
+        MachineName                    .                                                                    
+        ServicesDependedOn             {RPCSS, http}                                                        
+        CanShutdown                    False                                                                
+        Status                         Running                                                              
+        DependentServices              {Fax}                                                                
+        StartType                      Automatic                                                            
+
+    .NOTES
+        Created by David Christian
+    .Link
+        https://github.com/dchristian3188
+    #>
+
+    [CmdletBinding()]
+    [OutputType([HashTable])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true, 
+                   ValueFromPipelineByPropertyName=$true, 
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $InputObject,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Switch]
+        $IncludeNullValues=$false
+    )
+    Begin
+    {
+    }
+    Process
+    {
+        ForEach($object in $InputObject)
+        {
+            $propertyHash = @{}
+            $object.PSobject.Properties.Name.ForEach{
+                $propertyName = $PSItem
+                $currentValue = $object.$propertyName
+
+                If($IncludeNullValues)
+                {
+                    $propertyHash[$propertyName] = $object.$propertyName
+                }
+                ElseIf(![System.String]::IsNullOrEmpty($currentValue))
+                {
+                    $propertyHash[$propertyName] = $object.$propertyName
+                }
+            }
+            Write-Output -InputObject $propertyHash
+        }
+    }
+    End
+    {
+    }
+}
---
+++ Functions/ConvertTo-WhereString.ps1
@@ -0,0 +1,59 @@
+﻿Function ConvertTo-WhereString
+{
+    <#
+    .Synopsis
+       Converts a hashtable to a where script block.
+    .DESCRIPTION
+       Converts hashtable to filter script block. Useful
+       for creating where filters when not all properties
+       are known. 
+    .PARAMETER InputObject
+        Object to be converted
+    .EXAMPLE
+       
+       $whereScript = Get-Service -Name Spooler |
+            Select-Object Status,StartType,CanPauseAndContinue | 
+            ConvertTo-HashTable | 
+            ConvertTo-WhereString
+       
+       Get-Service |
+        Where-Object $whereScript
+      
+       Displays all services currenlty matching the Status, StartType and 
+       CanPauseAndContinue properties of the spooler service.
+
+    .NOTES
+        Created by David Christian
+    .Link
+        https://github.com/dchristian3188
+    #>
+
+    [CmdletBinding()]
+    [OutputType([ScriptBlock])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true, 
+                   ValueFromPipelineByPropertyName=$true, 
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [HashTable]
+        $InputHash
+    )
+    Begin
+    {
+    }
+    Process
+    {
+        $raw = ForEach($property in $InputHash.Keys)
+        {
+            Write-Output -InputObject ('($_.{0} -eq "{1}")' -f $property,$InputHash[$property])
+        }
+
+        $whereFilter = $raw -join " -and "
+        Return [ScriptBlock]::Create($whereFilter)
+    }
+    End
+    {
+    }
+}
