f883c1d457a3ba629d34699aef662fbd732366f9
dchristian3188 <dchritsian3188@gmail.com>
2016-09-12 21:42:27 -0700

    Cleaned up Get
    Converted get to loop on property names

--- DSC/NetFirewallProfile/NetFirewallProfile.psm1
+++ DSC/NetFirewallProfile/NetFirewallProfile.psm1
@@ -93,9 +93,11 @@ class NetFirewallProfile
     [bool] Test()
     {        
         $propertyHash = $this.ConvertoHash()
+        
         $whereString = $this.ConvertToWhere($propertyHash)
-        Write-Verbose -Message "Testing Profile: $($this.Name) with Filter: $($whereString)"
         $whereFilter = [scriptblock]::Create($whereString)
+        Write-Verbose -Message "Testing Profile: $($this.Name) with Filter: $($whereString)"
+        
         $foundProfile = Get-NetFirewallProfile | 
             Where-Object $whereFilter
         If($foundProfile)
@@ -111,23 +113,19 @@ class NetFirewallProfile
     [NetFirewallProfile] Get()
     {   
         $currentState = Get-NetFirewallProfile -Name $this.Name
-        
-        $this.Enabled = [GpoBoolean]::$($currentState.Enabled)     
-        $this.DefaultInboundAction = [Action]::$($currentState.DefaultInboundAction)
-        $this.DefaultOutboundAction = [Action]::$($currentState.DefaultOutboundAction)
-        $this.AllowInboundRules = [Action]::$($currentState.AllowInboundRules)
-        $this.AllowLocalFirewallRules = [Action]::$($currentState.AllowLocalFirewallRules)
-        $this.AllowLocalIPsecRules = [Action]::$($currentState.AllowLocalIPsecRules)
-        $this.AllowUserApps = [Action]::$($currentState.AllowUserApps)
-        $this.AllowUserPorts = [Action]::$($currentState.AllowUserPorts)
-        $this.AllowUnicastResponseToMulticast = [Action]::$($currentState.AllowUnicastResponseToMulticast)
-        $this.NotifyOnListen = [GpoBoolean]::$($currentState.NotifyOnListen)
-        $this.EnableStealthModeForIPsec = [GpoBoolean]::$($currentState.EnableStealthModeForIPsec)
-        $this.LogFileName = $currentState.LogFileName
-        $this.LogMaxSizeKilobytes = $currentState.LogMaxSizeKilobytes
-        $this.LogAllowed = [GpoBoolean]::$($currentState.LogAllowed)
-        $this.LogBlocked = [GpoBoolean]::$($currentState.LogBlocked)
-        $this.DisabledInterfaceAliases = $currentState.DisabledInterfaceAliases
+        $classDefinition = $this | Get-Member
+
+        ForEach($property in $classDefinition.Where{$PSItem.Membertype -eq "Property"})
+        {
+            $propertyName = $property.Name
+            Switch($property.Definition)
+            {
+                {$PSItem -match 'Action'} {$this.$propertyName = [Action]::$($currentState.$propertyName).ToString()}
+                {$PSItem -match 'GpoBoolean'} {$this.$propertyName = [GpoBoolean]::$($currentState.$propertyName).ToString()}
+                Default {$this.$propertyName = $currentState.$propertyName}
+            }
+        }
+
         return $this
     }
 
