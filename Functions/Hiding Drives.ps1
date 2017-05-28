dc6ce2cfe557065f813f128f3e549bce7e95431d
dchristian3188 <dchristian3188@gmail.com>
2017-05-28 11:33:33 -0700

    Drives

--- Functions/Hiding Drives.ps1
+++ Functions/Hiding Drives.ps1
@@ -4,20 +4,60 @@ Function Get-LetterMap
     1..26 | ForEach-Object {
         $currentNumber = $_
         $letter = [char]($currentNumber + 64)
-        $driveValue = [math]::Pow(2,($currentNumber-1))
+        $driveValue = [math]::Pow(2, ($currentNumber - 1))
         $letterMap[$letter] = $driveValue
     }
     Write-Output $letterMap
 }
 
-$HIDDEN_DRIVES_KEY = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\'
-$hiddenDrives = (Get-Item -Path $HIDDEN_DRIVES_KEY).GetValue("NoDrives")
-
-foreach($letter in $letterMap.Keys | Sort-Object)
+Function Get-HiddenDrive
 {
-    $driveHidden = ($hiddenDrives -band $letterMap[$letter]) -eq $letterMap[$letter]
-    If($driveHidden)
+    $HIDDEN_DRIVES_KEY = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\'
+    $hiddenDrives = (Get-Item -Path $HIDDEN_DRIVES_KEY).GetValue("NoDrives")
+
+    $letterMap = Get-LetterMap
+    foreach ($letter in $letterMap.Keys | Sort-Object)
     {
-        $letter
+        $driveHidden = ($hiddenDrives -band $letterMap[$letter]) -eq $letterMap[$letter]
+        If ($driveHidden)
+        {
+            $letter
+        }
+    }
+}
+
+Function Get-HiddenDrive
+{
+    $HIDDEN_DRIVES_KEY = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\'
+    $hiddenDrives = (Get-Item -Path $HIDDEN_DRIVES_KEY).GetValue("NoDrives")
+
+    $letterMap = Get-LetterMap
+    foreach ($letter in $letterMap.Keys | Sort-Object)
+    {
+        $driveHidden = ($hiddenDrives -band $letterMap[$letter]) -eq $letterMap[$letter]
+        If ($driveHidden)
+        {
+            $letter
+        }
+    }
+}
+
+function 
+{
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin
+    {
+    }
+    
+    process
+    {
+    }
+    
+    end
+    {
     }
 }
