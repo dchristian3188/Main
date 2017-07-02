e35bda07030c06aab12824941eea7e2ba24aaa9f
dchristian3188 <dchristian3188@gmail.com>
2017-07-01 22:31:01 -0700

    updated to get specific date

--- Functions/Get-SpecificDate.ps1
+++ Functions/Get-SpecificDate.ps1
@@ -1,43 +1,73 @@
 ﻿function Get-SpecificDate
 {
+
+    <#
+    .SYNOPSIS
+        Finds a specific instance of the day of the week.
+    .DESCRIPTION
+        This function can be used to find an instance of a day of the week.
+        For example, you can ask for the first monday of the month.
+    .PARAMETER Instance
+        Which week day instance to return for selected month.
+        Valid options are: First, Second, Third, Fourth, Fifth
+    .PARAMETER Day
+        Day of week to return. Options should be DayOfWeek type.
+    .PARAMETER Month
+        Which month to select. Value should be between 1 and 12.
+        Defaults to current month.
+    .PARAMETER Year
+        Whcih year to select. Default value is current year.
+    .EXAMPLE
+        PS C:\> Get-SpecifcDay -Instance Second -Day Tuesday
+        Returns a DateTime object representing the second tuesday of the 
+        current month
+    .OUTPUTS
+        DateTime
+    .Link
+        overpowershell.com
+    #>
+
     [CmdletBinding()]
-    [OutputType([System.DateTime])]
+    [OutputType([DateTime])]
     Param
     (
         
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        [ValidateSet("First", "Second", "Third","Fourth","Fifth")]
-        [System.String]
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        [ValidateSet("First", "Second", "Third", "Fourth", "Fifth")]
+        [string]
         $Instance,
 
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=1)]
-        [System.DayOfWeek]
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
+        [DayOfWeek]
         $Day,
 
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                   Position=2)]
-        [ValidateRange(1,12)]
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            Position = 2)]
+        [ValidateRange(1, 12)]
         [int]
         $Month = (Get-Date).Month,
 
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                   Position=2)]
+        [Parameter(
+            ValueFromPipelineByPropertyName = $true,
+            Position = 2)]
         [ValidateNotNullOrEmpty()]
         [int]
         $Year = (Get-Date).Year
-
     )
 
-    [System.DateTime]$TempDate = "{0}/{1}/{2}" -f $Year,$Month,1
+    [datetime]$tempDate = "{0}/{1}/{2}" -f $Year, $Month, 1
     
-    While($TempDate.DayOfWeek -ne $Day){
-        $TempDate = $TempDate.AddDays(1) 
+    while ($tempDate.DayOfWeek -ne $Day)
+    {
+        $tempDate = $tempDate.AddDays(1) 
     }
-
     
     $increment = switch ($Instance)
     {
@@ -48,14 +78,14 @@
         'Fifth' {28}
     }
 
-    $finalDate = $TempDate.AddDays($increment)
-    if($finalDate.Month -gt $Month){
-        Write-Warning -Message ("There is no {0} {1} in {2} ({3})" -f $Instance,$Day,[System.Globalization.DateTimeFormatInfo]::CurrentInfo.GetMonthName($Month),$Year)
-    }Else{
+    $finalDate = $tempDate.AddDays($increment)
+    if ($finalDate.Month -gt $Month)
+    {
+        $message = ("There is no {0} {1} in {2} ({3})" -f $Instance, $Day, [Globalization.DateTimeFormatInfo]::CurrentInfo.GetMonthName($Month), $Year)
+        throw [IndexOutOfRangeException]::New($message)
+    }
+    Else
+    {
         $finalDate
     }
 }
-
-
-$day = Get-SpecificDate -Instance fourth -Day tuesday
-$day
