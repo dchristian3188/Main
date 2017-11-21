6ec60c84d672f92b164adde0bb45593629c01ed8
dchristian3188 <dchristian3188@gmail.com>
2017-11-20 20:11:16 -0800

    Starting on Handbrake stuff

---
+++ Functions/Start-Handbrake.ps1
@@ -0,0 +1,23 @@
+$handbrakePath = 'C:\Program Files\HandBrake\HandBrakeCLI.exe'
+
+Foreach($file in $files)
+$source = "E:\asdfasdf4"
+$dest = "E:\Oadsfadsfmkv"
+
+$procArgs = '-i "{0}" -o "{1}" -Z "Very Fast 576p25" -f av_mkv' -f $source,$dest
+
+$results = Start-Process -FilePath $handbrakePath -ArgumentList $procArgs -NoNewWindow -Wait -PassThru
+
+
+
+Function Get-VideoLength($FilePath)
+{
+    $Folder = Split-Path -Path $FilePath
+    $File = Split-Path -Path $FilePath -Leaf
+    $LengthColumn = 27
+    $objShell = New-Object -ComObject Shell.Application 
+    $objFolder = $objShell.Namespace($Folder)
+    $objFile = $objFolder.ParseName($File)
+    $Length = $objFolder.GetDetailsOf($objFile, $LengthColumn)
+    $Length
+}
