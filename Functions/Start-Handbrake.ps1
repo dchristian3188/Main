110ff6149a9ff7936b9453bd8a821f71c3d0d433
dchristian3188 <dchristian3188@gmail.com>
2017-11-20 23:42:57 -0800

    More robust handbrake

--- Functions/Start-Handbrake.ps1
+++ Functions/Start-Handbrake.ps1
@@ -25,27 +25,29 @@ Function Start-Handbrake
     )
     begin
     {
-        if (-not  (Test-Path -Path $HandbrakePath))
+        if (-not  (Test-Path -LiteralPath $HandbrakePath))
         {
-            #Write-Error -Message "Unable to find Handbrake at path [$HandbrakePath]" -ErrorAction Stop
+            Write-Error -Message "Unable to find Handbrake at path [$HandbrakePath]" -ErrorAction Stop
         }
     }
     process
     {
         foreach ($file in $Path)
         {
-            if (-not(Test-Path -Path $file))
+            Write-Verbose -Message "Starting on file $file"
+            if (-not(Test-Path -LiteralPath $file))
             {
                 Write-Warning -Message "Unable to find file at path [$file]"
                 continue  
             }
             
-            $source = (Resolve-Path -Path $file).ProviderPath
+            $source = (Resolve-Path -LiteralPath $file).ProviderPath
             $dest = [IO.Path]::ChangeExtension($source, ".$($OutputFormat)")
             
             $procArgs = '-i "{0}" -o "{1}" -Z "{2}" -f av_{3}' -f $source, $dest, $HandbrakePreset, $OutputFormat
-            $procargs
-            #Start-Process -FilePath $HandbrakePath -ArgumentList $procArgs
+            Write-Verbose -Message "Running Handbrake with paramrs $($procArgs)"
+            Start-Process -FilePath $HandbrakePath -ArgumentList $procArgs -Wait
+            
             [pscustomobject]@{
                 Source       = $source
                 Destination  = $dest
@@ -59,6 +61,7 @@ Function Start-Handbrake
 
 Function Get-VideoLength($FilePath)
 {
+    
     $Folder = Split-Path -Path $FilePath
     $File = Split-Path -Path $FilePath -Leaf
     $LengthColumn = 27
@@ -69,41 +72,71 @@ Function Get-VideoLength($FilePath)
     $Length
 }
 
-Function Write-Warning($Message)
+
+Function Convert-Files
 {
-    Write-Warning -Message $Message
-}
+    [cmdletbinding()]
+    Param(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [string[]]
+        $Path
 
-$files = "somefiles"
-foreach ($file in $files)
-{
-    $hb = Start-Handbrake -Path $file 
-    if ($hb.LastExitCode -ne 0)
+    )
+    begin {}
+    process
     {
-        Write-Warning -Message "Filename [$file], last exit code not 0, [$($hb.LastExitCode)]"
-        continue
-    }
-    $orginalVideoLength = Get-VideoLength -FilePath $hb.Source
-    $newVideoLength = Get-VideoLength -FilePath $hb.Destination
-    $timeDifference = New-TimeSpan -Start $newVideoLength -End $orginalVideoLength
-    if ([math]::Abs($timeDifference.TotalSeconds) -gt 4)
-    {
-        Write-Warning -Message "Time duration doesnt match. Orginal [$orginalVideoLength] New [$newVideoLength]"
-        continue
-    }
+        foreach ($file in $Path)
+        {
+            $hb = Start-Handbrake -Path $file -Verbose
+            $hb | FL *
+            if ($hb.LastExitCode -ne 0)
+            {
+                Write-Warning -Message "Filename [$file], last exit code not 0, [$($hb.LastExitCode)]"
+                continue
+            }
 
-    $orginalSize = (Get-ChildItem -Path $hb.Source).Length
-    $newSize = (Get-ChildItem -Path $hb.Destination).Length
-
-    $newFileSmaller = $newSize -lt $orginalSize
-    if ($newFileSmaller)
-    {
-        Write-Verbose -Message "Orginal File is larger, removing"
-        Remove-Item -Path $hb.Source
+            if (-not(Test-Path -LiteralPath $hb.Destination))
+            {
+                Write-Warning -Message "Destination not found at [$($hb.Destination)]"
+                continue
+            }
+            $orginalVideoLength = Get-VideoLength -FilePath $hb.Source
+            $newVideoLength = Get-VideoLength -FilePath $hb.Destination
+            try
+            {
+                $timeDifference = New-TimeSpan -Start $newVideoLength -End $orginalVideoLength -ErrorAction Stop
+            }
+            catch
+            {
+                Write-Warning -Message "Unable to compare Video lengths... Exiting"
+                continue
+            }
+            if ([math]::Abs($timeDifference.TotalSeconds) -gt 4)
+            {
+                Write-Warning -Message "Time duration doesnt match. Orginal [$orginalVideoLength] New [$newVideoLength]"
+                continue
+            }
+        
+            $orginalSize = (Get-ChildItem -Path $hb.Source).Length
+            $newSize = (Get-ChildItem -Path $hb.Destination).Length
+        
+            Write-Verbose -Message ("Old Size [{0:N2}mb], new size [{1:N2}mb]" -f ($orginalSize / 1mb), ($newSize / 1mb))
+            $newFileSmaller = ($newSize -lt $orginalSize) -and ($newSize -gt 0)
+            if ($newFileSmaller)
+            {
+                Write-Verbose -Message "Orginal File is larger, removing"
+                Remove-Item -Path $hb.Source
+            }
+            else
+            {
+                Write-Verbose -Message "New File is larger, removing"
+                Remove-Item -Path $hb.Destination
+            }
+        }
     }
-    else
-    {
-        Write-Verbose -Message "New File is larger, removing"
-        Remove-Item -Path $hb.Destination
-    }
+    end {}        
 }
